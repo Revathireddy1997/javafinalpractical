a0ff47659c3713fa97d8f07ea2bab7adae349e72
revathi <revathireddy97@gmail.com>
2023-12-09 12:02:08 -0500

    created class for binary,additon, mul and modulo

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 // Abstract class to represent arithmetic expressions.
-abstract class ArithmeticExp {
+abstract class ArithmeticExpression {
    // Abstract method to be implemented by subclasses
    public abstract int evaluate();
    // Abstract method to be implemented by subclasses
@@ -9,21 +9,73 @@ abstract class ArithmeticExp {
    }
 }
 // Concrete class representing a number
-class ModifiedNumberExp extends ArithmeticExp {
+class NumberExp extends ArithmeticExpression {
    // Private field to store the value of the number
-   private int modifiedValue;
+   private int Value;
    // Constructor to initialize the value
-   public ModifiedNumberExp(int modifiedValue) {
-      this.modifiedValue = modifiedValue;
+   public NumberExp(int modifiedValue) {
+      this.Value = modifiedValue;
    }
    // Implementation of the evaluate method
    @Override
    public int evaluate() {
-      return modifiedValue;
+      return Value;
    }
    // Implementation of the toStringRepresentation method
    @Override
    public String toStringRepresentation() {
-      return String.valueOf(modifiedValue);
+      return String.valueOf(Value);
    }
 }
+// Abstract class representing binary operations
+abstract class BinaryExpression extends ArithmeticExpression {
+   protected ArithmeticExpression left;
+   protected ArithmeticExpression right;
+   public BinaryExpression(ArithmeticExpression left, ArithmeticExpression right) {
+      this.left = left;
+      this.right = right;
+   }
+}
+// Class representing addition
+class SumExp extends BinaryExpression {
+   public SumExp(ArithmeticExpression left, ArithmeticExpression right) {
+      super(left, right);
+   }
+   @Override
+   public int evaluate() {
+      return left.evaluate() + right.evaluate();
+   }
+   @Override
+   public String toStringRepresentation() {
+      return left.toStringRepresentation() + " + " + right.toStringRepresentation();
+   }
+}
+// Class representing multiplication
+class ProductExp extends BinaryExpression {
+   public ProductExp(ArithmeticExpression left, ArithmeticExpression right) {
+      super(left, right);
+   }
+   @Override
+   public int evaluate() {
+      return left.evaluate() * right.evaluate();
+   }
+   @Override
+   public String toStringRepresentation() {
+      return left.toStringRepresentation() + " * " + right.toStringRepresentation();
+   }
+}
+// Class representing modulo
+class ModuloExp extends BinaryExpression {
+   public ModuloExp(ArithmeticExpression left, ArithmeticExpression right) {
+      super(left, right);
+   }
+   @Override
+   public int evaluate() {
+      return left.evaluate() % right.evaluate();
+   }
+   @Override
+   public String toStringRepresentation() {
+      return left.toStringRepresentation() + " % " + right.toStringRepresentation();
+   }
+}
+
