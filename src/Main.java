316bc71b8424c200501f7aae82a45c7f930a3d37
revathi <revathireddy97@gmail.com>
2023-12-09 11:40:04 -0500

    abstract class and created a class representinga number

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,29 @@
 // Abstract class to represent arithmetic expressions.
- abstract class ArithmeticExp {
-
-    // Abstract method to be implemented by subclasses for evaluating the expression.
-    public abstract int evaluate();
-    // Abstract method to be implemented by subclasses for generating a string representation.
-    public abstract String toStringRepresentation();
-    // This is an example of a method with a default implementation.
-    // It can be overridden by subclasses, but it is not mandatory to do so.
-    public void someDefaultMethod() {
-        // This method can contain default behavior that is common to all subclasses
-    }
+abstract class ArithmeticExp {
+   // Abstract method to be implemented by subclasses
+   public abstract int evaluate();
+   // Abstract method to be implemented by subclasses
+   public abstract String toStringRepresentation();
+   public void someDefaultMethod() {
+      // This method can contain default behavior that is common to all subclasses
+   }
+}
+// Concrete class representing a number
+class ModifiedNumberExp extends ArithmeticExp {
+   // Private field to store the value of the number
+   private int modifiedValue;
+   // Constructor to initialize the value
+   public ModifiedNumberExp(int modifiedValue) {
+      this.modifiedValue = modifiedValue;
+   }
+   // Implementation of the evaluate method
+   @Override
+   public int evaluate() {
+      return modifiedValue;
+   }
+   // Implementation of the toStringRepresentation method
+   @Override
+   public String toStringRepresentation() {
+      return String.valueOf(modifiedValue);
+   }
 }
